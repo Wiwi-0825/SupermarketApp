57efc4b218d0edc2b35fa9472575835a344e631b
Wiwi-0825 <24049615@myrp.edu.sg>
2025-07-16 09:31:01 +0800

    testing123

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express'); //test
+const express = require('express'); 
 const mysql = require('mysql2');
 const session = require('express-session');
-const flash = require('connect-flash');
+const flash = require('connect-flash'); //testing
 const multer = require('multer');
 const app = express();
 
